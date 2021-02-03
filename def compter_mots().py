d14dba7ba20cdd558b34f730960269d9b446cd2e
stephanieR34 <34.regnier.stephanie@gmail.com>
2021-02-03 18:12:37 +0100

    def deux en commentaire

--- def compter_mots().py
+++ def compter_mots().py
@@ -7,27 +7,27 @@ def compter_mots(mots):
             repeat +=1
     return(repeat)
 
-def safa_count(input):
-  l = []
-  words = input.split()
-  for word in words:
-    if word not in l and words.count(word) > 1 :
-      l.append(word) 
-  return len(l)
-
-# def count_repeated_word(n, words):
-#   # on crée un set, un ensemble d'elements uniques
-#   s = set()
-#   # on split words, mot par mot
-#   words = words.split()
-#   # pour chaque mot dans la liste de mots
+# def safa_count(input):
+#   l = []
+#   words = input.split()
 #   for word in words:
-#     # si le mot est plus d'une fois dans la liste de mots
-#     if words.count(word) > 1:
-#       # on ajoute le mot dans notre set
-#       s.add(word)
-#   # on retourne la taille de s
-#   return len(s)
+#     if word not in l and words.count(word) > 1 :
+#       l.append(word) 
+#   return len(l)
+
+def count_repeated_word(n, words):
+  # on crée un set, un ensemble d'elements uniques
+  s = set()
+  # on split words, mot par mot
+  words = words.split()
+  # pour chaque mot dans la liste de mots
+  for word in words:
+    # si le mot est plus d'une fois dans la liste de mots
+    if words.count(word) > 1:
+      # on ajoute le mot dans notre set
+      s.add(word)
+  # on retourne la taille de s
+  return len(s)
 
 def count_repeat(mots):
     liste = mots.split()
