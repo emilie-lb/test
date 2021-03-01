f1adf7e0ad8eb539a987f3ee5093b16a1e64f769
Sarah <sarah.truffertgf2015@gmail.com>
2021-03-01 16:47:04 +0100

    ajout folder

---
+++ Afichageprint.py
@@ -0,0 +1,8 @@
+#Saut de ligne
+print("foo\nbar")
+
+#sep="" ça va être ce qui est entre les string que tu mets dans ton print 
+print("foo","bar",sep="->")
+
+#End va afficher end a la fin de ton print :
+print ("foo",end="miam")
